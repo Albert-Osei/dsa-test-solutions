e79c66ce87ee7074aa5f2b6f1f143d080c55f822
Albert-Osei <oseialbert072@gmail.com>
2022-09-30 12:36:58 +0000

    add first solution

---
+++ README.md
@@ -0,0 +1,18 @@
+# 👋 Welcome to dsa-test-solutions
+
+> A set of solutions for data structures and algorithms problems
+
+### 🏡 [Homepage](https://github.com/Albert-Osei/dsa-test-solutions#readme)
+
+## Techologies
+
+- NodeJS
+
+## Author
+👤 **Albert-Osei**
+
+## 📝 License
+
+Copyright @ 2022 [Albert-Osei](https://github.com/Albert-Osei)
+
+This project is [ISC] licensed
---
+++ easy/index.js
@@ -0,0 +1,34 @@
+/**
+ * PROBLEM 1: The Roman to Integer
+ * Given a roman numeral, convert it to an integer
+ * @param {String} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+
+    const sym = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    let result = 0
+    for (let i=0; i<s.length; i++) {
+        const cur = sym[s[i]]
+        const next = sym[s[i+1]]
+
+        if (cur < next) {
+            result += next - cur // IV = 5 - 1 = 4
+            i++
+        } else {
+            result += cur
+        }
+    }
+    return result
+}
+
+console.log(romanToInt('III'))
